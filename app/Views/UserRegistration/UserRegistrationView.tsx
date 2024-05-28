ca601f005d01102a29abca25f431917af2a6aca5
Ian Pacini <ianpacini@hotmail.com>
2024-05-27 21:02:15 -0300

    quick update

--- app/Views/UserRegistration/UserRegistrationView.tsx
+++ app/Views/UserRegistration/UserRegistrationView.tsx
@@ -2,47 +2,57 @@
 
 import React from 'react';
 import TextField from '../GeneralComponents/TextField';
+import GreenButton from '../GeneralComponents/GreenButton';
 
 const UserRegistationView: React.FC = () => {
     return (
-        <div style={{ width: '1600px', height: '680px' }} className="bg-white flex flex-col items-center rounded-2xl justify-center p-8 m-20">
-            <div className='bg-black items-start w-full'>
-                <div className='flex'>
-                    <div className='mx-8'>
+        <div className="bg-white flex flex-col items-center rounded-2xl justify-center p-8 m-20">
+            <text className='text-black text-2xl font-bold mb-20'>Registre-se como empreendedor</text>
+            
+            <div className='flex items-start justify-between w-full'>
+                <GreenButton label={"Voltar"}/>
+                
+                <div/>
+            </div>
+            <div className='items-start w-full'>
+                <div className='flex mb-12'>
+                    <div>
                     <TextField label='' placeholder=''/>
                     </div>
-                    <div className='mx-8'>
-                    <TextField label='' placeholder=''/>
-                    </div>
-                </div>
-                <div className='flex'>
-                    <div className='mx-8'>
-                    <TextField label='' placeholder=''/>
-                    </div>
-                    <div className='mx-8'>
-                    <TextField label='' placeholder=''/>
-                    </div>
-                    <div className='mx-8'>
+                    <div className='ml-10'>
                     <TextField label='' placeholder=''/>
                     </div>
                 </div>
-                <div className='flex'>
-                    <div className='mx-8'>
+                <div className='flex mb-12'>
+                    <div>
                     <TextField label='' placeholder=''/>
                     </div>
-                    <div className='mx-8'>
+                    <div className='mx-10'>
+                    <TextField label='' placeholder=''/>
+                    </div>
+                    <div>
                     <TextField label='' placeholder=''/>
                     </div>
                 </div>
-                <div className='flex'>
-                    <div className='mx-8'>
+                <div className='flex mb-12'>
+                     <div>
                     <TextField label='' placeholder=''/>
                     </div>
-                    <div className='mx-8'>
+                    <div className='ml-10'>
+                    <TextField label='' placeholder=''/>
+                    </div>
+                </div>
+                <div className='flex mb-20'>
+                    <div>
+                    <TextField label='' placeholder=''/>
+                    </div>
+                    <div className='ml-10'>
                     <TextField label='' placeholder=''/>
                     </div>
                 </div>
             </div>
+
+            <GreenButton label={"Registrar"}/>
         </div>
     );
 };
